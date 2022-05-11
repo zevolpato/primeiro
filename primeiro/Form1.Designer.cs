db9206d80ceb498ad9354de80433f272bfd513c7
Jose Afranio Volpato Junior <jose.avjunior@sp.senac.br>
2022-05-11 14:58:42 -0300

    primeiro

--- primeiro/Form1.Designer.cs
+++ primeiro/Form1.Designer.cs
@@ -112,7 +112,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(290, 252);
             this.Controls.Add(this.btnMultiplicacao);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
